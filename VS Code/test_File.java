2963d05ea783258acbfff9d6615e86ddb8cd3a9b
ENOSISBD\sizer <sizerahmad@gmail.com>
2023-01-04 17:31:13 +0600

    Print line updated.

--- VS Code/test_File.java
+++ VS Code/test_File.java
@@ -2,7 +2,7 @@ public class test_File{
         public static void main(String[] args){
 
             for (int i=0; i<11; i++){
-                System.out.println("Moinul pagla "+i);
+                System.out.println("Github test "+i);
             }   
     }
 }
