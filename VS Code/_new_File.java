f00a21fe547d134e1bcc9efca9e5437562af10de
ENOSISBD\sizer <sizerahmad@gmail.com>
2023-01-04 17:19:26 +0600

    Java test file added.

---
+++ VS Code/_new_File.java
@@ -0,0 +1,7 @@
+public class _new_File
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Abdul Bari");       
+    }
+}
---
+++ VS Code/test_File.java
@@ -0,0 +1,8 @@
+public class test_File{
+        public static void main(String[] args){
+
+            for (int i=0; i<11; i++){
+                System.out.println("Moinul pagla "+i);
+            }   
+    }
+}
